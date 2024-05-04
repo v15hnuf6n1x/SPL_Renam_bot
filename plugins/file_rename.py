0486b49e0be41f16fc520f67d7d1742a62204401
v15hnuf6n1off <145260326+v15hnuf6n1off@users.noreply.github.com>
2024-05-04 12:04:57 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -81,8 +81,9 @@ async def doc(bot, update):
     prefix = await db.get_prefix(update.message.chat.id)
     suffix = await db.get_suffix(update.message.chat.id)
     new_name = update.message.text
-    new_filename_ = new_name.split(":-")[1]
-    new_filename_ = new_filename.replace("_"," ")
+    new_filen = new_name.split(":-")[1]
+    new_filename_ = new_filen.replace("_"," ")
+    print("filename after replacing _ :",new_filename)
 
     try:
         # adding prefix and suffix
