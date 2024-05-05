e2a2b5bf5c1cd67bf8dfcbde703b0a7e2083a5a7
v15hnuf6n1off <145260326+v15hnuf6n1off@users.noreply.github.com>
2024-05-05 23:07:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,36 +72,21 @@ class Bot(Client):
 
 bot_instance = Bot()
 
-async def main():
-    if Config.STRING_SESSION:
-        await asyncio.gather(
-            app.start(),        # Start the Pyrogram Client
-            bot_instance.start()  # Start the bot instance
-        )
-    else:
-        await bot_instance.start()
+def main():
+    async def start_services():
+        if Config.STRING_SESSION:
+            await asyncio.gather(
+                app.start(),        # Start the Pyrogram Client
+                bot_instance.start()  # Start the bot instance
+            )
+        else:
+            await asyncio.gather(
+                bot_instance.start()
+            )
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(start_services())
 
 if __name__ == "__main__":
     warnings.filterwarnings("ignore", message="There is no current event loop")
-    asyncio.run(main())
-
-
-#def main():
-#    async def start_services():
-#        if Config.STRING_SESSION:
-#            await asyncio.gather(
-#                app.start(),        # Start #the Pyrogram Client
-               # bot_instance.start()  # Start #the bot instance
-#            )
-#        else:
-#            await asyncio.gather(
-#                bot_instance.start()
-#            )
-        
-#    loop = asyncio.get_event_loop()
- #   loop.run_until_complete(start_services())
-#    loop.run_forever()
-
-#if __name__ == "__main__":
-#    warnings.filterwarnings("ignore", #message="There is no current event loop")
-#    main()
+    main()
